e9ba19dc0863678e2a3400efaa6591b260ccc1f8
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-10-17 16:28:53 +0000

    ComputeAverageScoreForUser procedure

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,22 +1,20 @@
 -- Create a stored procedure ComputeAverageScoreForUser that computes and stores the average score for a student
 
-DELIMITER $$  -- Change the delimiter to allow for multi-line statements
+DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser (
-    IN user_id INT  -- User ID (linked to an existing user in the users table)
-)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10, 2);  -- Variable to hold the average score
+    DECLARE avg_score FLOAT;
 
-    -- Calculate the average score from the corrections table for the given user_id
+    -- Calculate the average score for the given user_id
     SELECT AVG(score) INTO avg_score
     FROM corrections
     WHERE user_id = user_id;
 
-    -- Update the average_score column in the users table
+    -- Update the user's average_score in the users table
     UPDATE users
     SET average_score = avg_score
     WHERE id = user_id;
-END$$
+END //
 
-DELIMITER ;  -- Reset the delimiter back to default
+DELIMITER ;
